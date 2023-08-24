616ceafff4e1462d1019b86d8b60272ec2204f5a
justin-chiang <59491073+justin-chiang@users.noreply.github.com>
2023-08-24 13:51:11 -0700

    finished calendar fetch functionality

--- gservices.py
+++ gservices.py
@@ -1,4 +1,8 @@
 import os
+import json
+import pytz
+from datetime import datetime, timedelta
+from dateutil import tz
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -22,7 +26,27 @@ def create_service(api_name, api_version, cred_file, scopes):
 
     try:
         service = build(api_name, api_version, credentials=creds, static_discovery=False)
-        print(api_name, ' service built successfully')
+        print(api_name, 'service built successfully')
         return service
     except HttpError as err:
         print(err)
+
+def get_todays_events(service):
+    today = datetime.utcnow().date()
+    start = datetime(today.year, today.month, today.day, hour=7).isoformat() + 'Z'
+    end = (datetime(today.year, today.month, today.day, hour=7) + timedelta(1)).isoformat() + 'Z'
+
+    print(start)
+    print(end)
+
+    todays_events = []
+    page_token = None
+    while True:
+        events = service.events().list(calendarId='primary', timeMin=start, timeMax=end, maxResults=10, singleEvents=True, orderBy='startTime').execute()
+        for event in events['items']:
+            todays_events.append(event)
+        page_token = events.get('nextPageToken')
+        if not page_token:
+            break
+
+    return todays_events
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from gservices import create_service
+from dateutil import parser
+from gservices import create_service, get_todays_events
 
 CRED_FILE = 'credentials.json'
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/tasks.readonly']
 
 CALENDAR_API_NAME = 'calendar'
 CALENDAR_API_VERSION = 'v3'
-CALENDAR_SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 TASKS_API_NAME = 'tasks'
 TASKS_API_VERSION = 'v1'
-TASKS_SCOPES = ['https://www.googleapis.com/auth/tasks.readonly']
 
-calendar = create_service(CALENDAR_API_NAME, CALENDAR_API_VERSION, CRED_FILE, CALENDAR_SCOPES)
-tasks = create_service(TASKS_API_NAME, TASKS_API_VERSION, CRED_FILE, TASKS_SCOPES)
+calendar = create_service(CALENDAR_API_NAME, CALENDAR_API_VERSION, CRED_FILE, SCOPES)
+tasks = create_service(TASKS_API_NAME, TASKS_API_VERSION, CRED_FILE, SCOPES)
+
+calendar_overview = ''
+todays_events = get_todays_events(calendar)
+for event in todays_events:
+    name = event.get('summary')
+    start = event.get('start').get('dateTime')
+    end = event.get('end').get('dateTime')
+
+    if start != None and end != None:
+        start = parser.parse(start).strftime('%I:%M%p').lower()
+        end = parser.parse(end).strftime('%I:%M%p').lower()
+        calendar_overview += f'{start}-{end}: {name}\n'
+    else:
+        calendar_overview += f'all day: {name}\n'
+
+print(calendar_overview)
